69139d7b130d9f9130573796a556c141673470e7
Musaab Yousif <musaab.o.yousif@wmich.edu>
2018-05-15 15:22:00 -0400

    Added JSON package manager Added new class JSONProcessor

---
+++ MonsterScalerMarkI/MonsterScalerMarkI/JSONProcessor.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using Newtonsoft.Json.Serialization;
+
+namespace MonsterScalerMarkI
+{
+    class JSONProcessor
+    {
+        JsonReader jreader;
+        public void Read(string filename)
+        {
+            bool b = false;
+            if (b)
+            {
+                
+                jreader.Read();
+            }
+            else
+            {
+                filename = "Failure";
+            }
+        }
+    }
+}
